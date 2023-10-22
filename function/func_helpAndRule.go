4f974ab0128d5183dfca60e297aa0240e848507c
srivier <soleane.rivier@ynov.com>
2023-10-22 21:01:59 +0200

    feat : help and rules possible when --help or --rule

---
+++ function/func_helpAndRule.go
@@ -0,0 +1,41 @@
+package Hangman
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func Help() {
+	filePath := "Ressources/doc.txt"
+
+	pagerCommand := "less"
+
+	cmd := exec.Command(pagerCommand, filePath)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Erreur lors de l'exécution de %s : %v\n", pagerCommand, err)
+		os.Exit(1)
+	}
+}
+
+func Rules() {
+	filePath := "Ressources/rules.txt"
+
+	pagerCommand := "less"
+
+	cmd := exec.Command(pagerCommand, filePath)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Erreur lors de l'exécution de %s : %v\n", pagerCommand, err)
+		os.Exit(1)
+	}
+}
