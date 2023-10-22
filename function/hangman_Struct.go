7545d16c93773d9ad23ec032cbf1abf9d298f6c2
srivier <soleane.rivier@ynov.com>
2023-10-22 19:15:26 +0200

    feat : Add a hangman Structure

---
+++ function/hangman_Struct.go
@@ -0,0 +1,8 @@
+package Hangman
+
+type HangManData struct {
+	Word             []rune     // Word composed of '_', ex: H_ll_
+	ToFind           string     // Final word chosen by the program at the beginning. It is the word to find
+	Attempts         int        // Number of attempts left
+	HangmanPositions [10]string // It can be the array where the positions parsed in "hangman.txt" are stored
+}
