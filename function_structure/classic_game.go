d70ff8bd22b3c9333e2112330f40214fc2ec07b6
srivier <soleane.rivier@ynov.com>
2023-10-24 19:28:34 +0200

    docs : add comment to ClassicGame()

--- function_structure/classic_game.go
+++ function_structure/classic_game.go
@@ -8,22 +8,29 @@ func ClassicGame(data HangManData) {
 	var game HangManData = data
 	gameOver := false
 
-	for !gameOver {
+	for !gameOver { // Game loop
+		// Display word and attempts
 		printRune(game.Word)
-		fmt.Printf("Attention plus que %d mauvaise reponse accepté !\n", game.Attempts)
+		fmt.Printf("Attention plus que %d mauvaise(s) réponse(s) acceptée(s) !\n", game.Attempts)
 		letter := input("Entrez une lettre ou un mot :", inputs)
+
+		// Verify input
 		if game.meca(letter) {
 			gameOver = true
 		}
 
+		// Display hangman
 		if game.HangmanPositions >= 0 && game.HangmanPositions <= 9 {
 			game.displayHangmanClassic()
 		}
 
+		// Verify if it's the end of the game
 		if game.endGame() {
 			gameOver = true
 		}
 	}
+
+	// Announcement of results
 	if game.Attempts > 0 {
 		fmt.Println("Felicitation ! Vous avez gagné !")
 	} else {
