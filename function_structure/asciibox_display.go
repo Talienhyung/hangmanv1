6e96945c7d25ce6624d88e1b2c71e3f7687f60a1
srivier <soleane.rivier@ynov.com>
2023-10-24 19:08:07 +0200

    docs : Adds comment to both file functions

--- function_structure/asciibox_display.go
+++ function_structure/asciibox_display.go
@@ -2,9 +2,10 @@ package hangman
 
 import "github.com/nsf/termbox-go"
 
-func (data *Game) DisplayAscii(x, y, version int, borderColor termbox.Attribute) {
+// Displays a given ascii character in x y
+func (data *Game) displayAscii(x, y, version int, borderColor termbox.Attribute) {
 	var ascii [95][9]string
-	switch data.letterFile {
+	switch data.letterFile { //choose the right font for ascii art
 	case "shadow":
 		ascii = readAscii("Ressources/Ascii_Letter/shadow.txt")
 	case "standard":
@@ -14,8 +15,7 @@ func (data *Game) DisplayAscii(x, y, version int, borderColor termbox.Attribute)
 	default:
 		ascii = readAscii("Ressources/Ascii_Letter/standard.txt")
 	}
-
-	for i := 0; i <= 8; i++ {
+	for i := 0; i <= 8; i++ { //displays the correct character
 		runes := []rune(ascii[version-32][i])
 		for index, j := range runes {
 			termbox.SetCell(x+index, y+i, j, borderColor, termbox.ColorDefault)
@@ -23,19 +23,20 @@ func (data *Game) DisplayAscii(x, y, version int, borderColor termbox.Attribute)
 	}
 }
 
+// Displays the last letter entered by a user in the terminal, followed by the final result (win or lose).
 func (data *Game) asciiBox(word string) {
 	switch word {
-	case "win":
-		data.DisplayAscii(55+18, 15, 'I', termbox.ColorGreen)
-		data.DisplayAscii(55, 15, 'W', termbox.ColorGreen)
-		data.DisplayAscii(55+16+14, 15, 'N', termbox.ColorGreen)
-	case "lose":
-		data.DisplayAscii(56, 15, 'L', termbox.ColorRed)
-		data.DisplayAscii(55+11, 15, 'O', termbox.ColorRed)
-		data.DisplayAscii(55+16+5, 15, 'S', termbox.ColorRed)
-		data.DisplayAscii(55+16+14, 15, 'E', termbox.ColorRed)
-	default:
+	case "win": //display WIN if player win
+		data.displayAscii(55+18, 15, 'I', termbox.ColorGreen)
+		data.displayAscii(55, 15, 'W', termbox.ColorGreen)
+		data.displayAscii(55+16+14, 15, 'N', termbox.ColorGreen)
+	case "lose": //display lose if player lose
+		data.displayAscii(56, 15, 'L', termbox.ColorRed)
+		data.displayAscii(55+11, 15, 'O', termbox.ColorRed)
+		data.displayAscii(55+16+5, 15, 'S', termbox.ColorRed)
+		data.displayAscii(55+16+14, 15, 'E', termbox.ColorRed)
+	default: //displays the first rune of the last input
 		runes := []rune(word)
-		data.DisplayAscii(55+16, 15, int(runes[0]), termbox.ColorLightRed)
+		data.displayAscii(55+16, 15, int(runes[0]), termbox.ColorLightRed)
 	}
 }
