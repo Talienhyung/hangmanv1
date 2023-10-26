5da2f674e35c72c2fbd2765157f808ffd2df1124
srivier <soleane.rivier@ynov.com>
2023-10-26 09:41:01 +0200

    docs : comment all function

--- function_structure/initialization.go
+++ function_structure/initialization.go
@@ -2,6 +2,7 @@ package hangman
 
 import "math/rand"
 
+// Set HangMan's first Data
 func (hangman *HangManData) setData() {
 	hangman.Word = []rune{}
 	hangman.ToFind = ""
@@ -11,17 +12,21 @@ func (hangman *HangManData) setData() {
 	hangman.ListWord = []string{}
 }
 
+// Set Word and ToFind for HangManData
 func (hang *HangManData) SetWord(dico []string) {
+	// Find a random word
 	randomIndex := rand.Intn(len(dico) - 1)
 	hang.ToFind = dico[randomIndex]
-	nbVisibleLetter := len(hang.ToFind)/2 - 1
-	for range hang.ToFind {
+
+	nbVisibleLetter := len(hang.ToFind)/2 - 1 // Set the number of letters that will be visible
+
+	for range hang.ToFind { // Set Word
 		hang.Word = append(hang.Word, '_')
 	}
+
 	again := false
 	var place []int
-
-	for nbVisibleLetter > 0 {
+	for nbVisibleLetter > 0 { // Finds randomly different nbVisibleLetter in ToFind
 		randomIndex = rand.Intn(len(hang.ToFind))
 		for _, j := range place {
 			if j == randomIndex {
@@ -35,11 +40,11 @@ func (hang *HangManData) SetWord(dico []string) {
 			again = false
 		}
 	}
+
 	WordRune := []rune(hang.ToFind)
-	for _, index := range place {
+	for _, index := range place { // Add the different letter into Word
 		hang.Word[index] = WordRune[index]
 		hang.LetterInWord(WordRune[index])
 		hang.UsedLetter(WordRune[index])
 	}
-
 }
