cf3258e1741ac3aaf66e2c6edc5c56c8dfa5b64f
srivier <soleane.rivier@ynov.com>
2023-10-27 09:14:58 +0200

    fix : fix a input bug

--- function_structure/termbox_game.go
+++ function_structure/termbox_game.go
@@ -26,7 +26,7 @@ func Draw(data HangManData, game Game) {
 
 		// Display user input with a blinking cursor
 		drawText([]rune(userInput), 2, 10, termbox.ColorDefault, true)
-		drawText(HangMan.Word, 2, 5, termbox.ColorDefault, false)
+		drawText(HangMan.Word, 2, 4, termbox.ColorDefault, false)
 		drawText(HangMan.ListUsed, 2, 17, termbox.ColorDefault, false)
 		termbox.Flush()
 
@@ -35,18 +35,22 @@ func Draw(data HangManData, game Game) {
 		if ev.Type == termbox.EventKey {
 			if ev.Key == termbox.KeyEsc {
 				return // Exit the game loop
-			} else if (ev.Key == termbox.KeySpace || ev.Key == termbox.KeyEnter) && userInput != "" {
-				// Check if the user's input is a valid guess and update the word or game status
-				if HangMan.meca(userInput) {
-					word = "win"
-				} else {
-					word = userInput
+			} else if ev.Key == termbox.KeySpace || ev.Key == termbox.KeyEnter {
+				if userInput != "" {
+					// Check if the user's input is a valid guess and update the word or game status
+					if HangMan.meca(userInput) {
+						word = "win"
+					} else {
+						word = userInput
+					}
+					userInput = "" // Clear user input
 				}
-				userInput = "" // Clear user input
 			} else if ev.Key == termbox.KeyDelete {
 				userInput = "" // Clear user input
 			} else if ev.Key == termbox.KeyBackspace || ev.Key == termbox.KeyBackspace2 {
-				userInput = userInput[:len(userInput)-1] // Remove the last character from user input
+				if userInput != "" {
+					userInput = userInput[:len(userInput)-1] // Remove the last character from user input
+				}
 			} else {
 				userInput += string(ev.Ch) // Add the character to user input
 			}
