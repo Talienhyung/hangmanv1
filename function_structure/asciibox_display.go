9f53e2e3a6d94cdd51c45d9e1bf1abd352782098
srivier <soleane.rivier@ynov.com>
2023-10-27 09:53:12 +0200

    feat : Add displayAsciiText() to display a word in the cmd

--- function_structure/asciibox_display.go
+++ function_structure/asciibox_display.go
@@ -1,6 +1,10 @@
 package hangman
 
-import "github.com/nsf/termbox-go"
+import (
+	"fmt"
+
+	"github.com/nsf/termbox-go"
+)
 
 // Displays a given ascii character in x y
 func (data *Game) displayAscii(x, y, version int, borderColor termbox.Attribute) {
@@ -38,5 +42,36 @@ func (data *Game) asciiBox(word string) {
 	default: //displays the first rune of the last input
 		runes := []rune(word)
 		data.displayAscii(55+16, 15, int(runes[0]), termbox.ColorLightRed)
+	}
+}
+
+// displayAsciiText displays ASCII art text using the 'letterFile' font.
+// The function selects the font based on 'letterFile' and displays the ASCII art text.
+func (data *Game) displayAsciiText(words []rune) {
+	// Define a 2D array 'ascii' to hold ASCII art characters.
+	var ascii [95][9]string
+
+	// Select the font for the ASCII art based on the 'letterFile' field.
+	switch data.letterFile {
+	case "shadow":
+		ascii = readAscii("Ressources/Ascii_Letter/shadow.txt")
+	case "standard":
+		ascii = readAscii("Ressources/Ascii_Letter/standard.txt")
+	case "thinkertoy":
+		ascii = readAscii("Ressources/Ascii_Letter/thinkertoy.txt")
+	default:
+		ascii = readAscii("Ressources/Ascii_Letter/standard.txt")
+	}
+
+	// Loop through each line of the ASCII art (9 lines in total).
+	for line := 0; line <= 8; line++ {
+		// Loop through each letter (rune) in the 'words' slice.
+		for _, letter := range words {
+			// Print the ASCII character for the current letter on the current line.
+			// The ASCII value of the letter is used to index 'ascii' array.
+			fmt.Printf(ascii[letter-32][line])
+		}
+		// After printing a line of text, move to the next line.
+		fmt.Println("")
 	}
 }
